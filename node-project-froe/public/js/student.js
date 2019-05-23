3319fb0d0fe75b21b6a1731b133851959e1c0bef
BaTiany <18296970472@163.com>
2019-05-23 20:42:06 +0800

    分页操作

--- node-project-froe/public/js/student.js
+++ node-project-froe/public/js/student.js
@@ -4,15 +4,22 @@ var Student = {
  * 
  */
   data:{
-    list: [],
+    list: [],// 当前学生集合
+    totalPage:1,//总页数
+    pageNum:1,//当前的页数
+    pageSize:10,//每页显示的条数
   },
   /**
    * 查询学生
    */
   findStudent: function () {
-    $.get('http://localhost:3000/api/stu', function (res) {
+    $.get('http://localhost:3000/api/stu',{
+      pageNum:Student.data.pageNum,
+      pageSize:Student.data.pageSize,
+    },  function (res) {
       if (res.code === 0){
         Student.data.list = res.data.list;
+        Student.data.totalPage = res.data.totalPage;
 
         let html = ejs.render(
           $('#a').html(),
@@ -21,12 +28,40 @@ var Student = {
           }
         )
 
+        let pageHtml = ejs.render(
+          $('#b').html(),
+          {
+            num:Student.data.totalPage,
+            pageNum: Student.data.pageNum
+          }
+        )
         $('#myTable tbody').html(html);
+        $('#myPage').html(pageHtml);
       }else{
         alert('网络异常，请稍后重试！')
       }
     })
-  }
+  },
+
+
+/**
+ * 
+ * 事件绑定
+ */
+bind: function () {
+  $('#myPage').on('click','li',function(){
+    //需要告诉我点的是第几页
+    let toPage = $(this).data('num'); //要去的页数
+    //判断 toPage 与 pageNum 不相同才去请求数据
+    if (toPage !== Student.data.pageNum) {
+      // 将 Student.data.pageNum 设置 为 toPage
+      Student.data.pageNum = toPage;
+      // 请求数据
+      Student.findStudent();
+    }
+  })
+}
+
 }
 
 
@@ -35,4 +70,6 @@ var Student = {
 $(function () {
   // 请求
   Student.findStudent();
+  //调用事件绑定
+  Student.bind();
 })
