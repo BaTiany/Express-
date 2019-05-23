69e05bd5a211d4982e8545eca0ed96d94edea65b
BaTiany <18296970472@163.com>
2019-05-23 19:44:10 +0800

    ejs浏览器端使用

--- node-project-froe/public/js/student.js
+++ node-project-froe/public/js/student.js
@@ -1,10 +1,30 @@
 var Student = {
+/**
+ * 需要用到的数据
+ * 
+ */
+  data:{
+    list: [],
+  },
   /**
    * 查询学生
    */
   findStudent: function () {
     $.get('http://localhost:3000/api/stu', function (res) {
-      console.log(res);
+      if (res.code === 0){
+        Student.data.list = res.data.list;
+
+        let html = ejs.render(
+          $('#a').html(),
+          {
+            list: Student.data.list
+          }
+        )
+
+        $('#myTable tbody').html(html);
+      }else{
+        alert('网络异常，请稍后重试！')
+      }
     })
   }
 }
